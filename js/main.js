a68ede1a308dca9ecf97541a65cea48b9121df8f
Mahmudur <srahmandiu@gmail.com>
2021-09-02 01:00:24 +0600

    Initialization

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Search Field Handle
+const searchField = document.getElementById('search-field');
+const searchBook = () => {
+    const searchText = searchField.value;
+    searchField.value = '';
+
+}
